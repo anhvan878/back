4743384452509f21e4223d68bac4a83ea833cce9
anhvan878 <olliaecheoneyg@hotmail.com>
2025-03-26 21:21:38 +0900

    test(core): add watch tests

--- watch.go
+++ watch.go
@@ -1,5 +1,15 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	fmt.Println("Hello, World!")
+	// Print greeting message with program name
+	if len(os.Args) > 1 {
+		fmt.Printf("Hello, %s!\n", os.Args[1])
+	} else {
+		fmt.Println("Hello, World!")
+	}
 }
